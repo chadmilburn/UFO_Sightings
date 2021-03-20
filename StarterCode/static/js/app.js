fa3f19b1c3c62558a8110188202282d1188ae51e
Chad Milburn <cmilburn@ballstate.bsu.edu>
2021-03-20 17:28:13 -0400

    got full table loading

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,19 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// select table element
+var tbody = d3.select("tbody");
+
+// build table
+tableData.forEach(sighting => {
+    // iterates all data
+    //console.log(sighting);
+    //add tr elements
+    var row = tbody.append("tr");
+    // add td elements fill with values from tableData
+    Object.entries(sighting).forEach(function ([key, value]) {
+        var cell = row.append("td")
+        cell.text(value)
+    });
+});
+
